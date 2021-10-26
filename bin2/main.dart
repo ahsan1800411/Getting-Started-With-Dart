0cbe42dc73030a1f50523dbecb49bfe1b54179b8
Ahsan <ahsanmumtaz008976@gmail.com>
2021-10-26 08:01:41 +0500

    Add files via upload

---
+++ bin2/main.dart
@@ -0,0 +1,85 @@
+void main() {
+  // print("Hello World");
+  // print("Welcome to Dart");
+  // String name = 'Ahsan';
+  // int age = 21;
+  // double height = 5.10;
+  // bool likesDart = true;
+  // print(name);
+  // print(age);
+  // print(height);
+  // print(likesDart);
+  // print('My name is $name and age is $age and my height is $height');
+  // double temp = 44.4;
+  // print('${temp}C');
+  // print('\\');
+
+  // String title = 'Dart Course';
+  // print(title.toUpperCase());
+  // print(title.toLowerCase());
+  // print(title.contains("Dart"));
+  // print(title.replaceAll("Dart", "JavaScript"));
+  // print(title.isEmpty);
+  // int age = 21;
+  // String mainString = age.toString();
+  // print(mainString);
+
+  // double height = 21.84444444;
+  // print(height.toStringAsFixed(1));
+
+  // String age = "21";
+  // print(double.parse(age));
+
+  // int x = 7;
+  // double y = x.toDouble();
+  // print(y);
+
+  // double F = 90;
+  // double FTC = (F - 32) / 1.8;
+  // print(FTC.toStringAsFixed(1));
+
+  // var email = 'ahsanmumtagmail.com';
+  // print(email.isNotEmpty || email.contains('@'));
+
+  // final salary=1500;
+  // const age=21;
+  // dynamic name='Ahsan';
+  // name=true;
+  // print(name);
+  // var i = 0;
+  // while (i <= 5) {
+  //   print('x' * i);
+  //   i++;
+  // }
+
+  // for (var i = 0; i < 5; i++) {
+  //   print('x' * i);
+  // }
+
+  // fizz buzz algorithm
+//   for (var i = 1; i <= 15; i++) {
+
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       print("fizz buzz");
+//     } else if (i % 3 == 0) {
+//       print("fizz");
+//     } else if (i % 5 == 0) {
+//       print("Buzz");
+//     } else {
+//       print(i);
+//     }
+//   }
+// print("Done");
+
+  // const medal =Medal.
+  // switch (position) {
+  //   case 2:
+  //     print("Gold");
+  //     break;
+  //   case 3:
+  //     print("Silver");
+  //     break;
+  //   default:
+  //     print("No medal");
+  // }
+}
